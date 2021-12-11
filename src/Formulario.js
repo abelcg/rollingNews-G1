7dc75e4fd7d5025d252045d9bab087c0c26c0b1f
Esteban Marquez <tebi_cba1@hotmail.com>
2021-12-11 01:13:46 -0300

    se termino de maquetar el formulario completo junto a sus validaciones correspondientes

--- src/Formulario.js
+++ src/Formulario.js
@@ -1,94 +1,103 @@
-import React from "react";
-import { Formik } from "formik";
-
+import React, { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 const Formulario = () => {
+  const [formularioEnv, cambiarFormularioEnv] = useState(false);
   return (
     <>
+	
       <Formik
-	  	initialValues={{
-			  nombre : "",
-			  correo : "",
-			  texto: ""
-		  }}
-		  validate={ (valores) => {
-			let errores = {};
+        initialValues={{
+          nombre: "",
+          correo: "",
+          texto: "",
+        }}
+        validate={(valores) => {
+          let errores = {};
 
-			//Validacion nombre
-			if(!valores.nombre){
-				errores.nombre = "Ingrese un nombre."
-			}else if(!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.nombre)){
-				errores.nombre= "El nombre solo puede contener espacios y letras."
-			}
+          //Validacion nombre
+          if (!valores.nombre) {
+            errores.nombre = "Ingrese un nombre.";
+          } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.nombre)) {
+            errores.nombre = "El nombre solo puede contener espacios y letras.";
+          }
 
-			//Validacion correo
-			if(!valores.correo){
-				errores.correo = "Ingrese un correo electrónico."
-			}else if(!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valores.correo)){
-				errores.correo= "El correo solo puede tener letras, puntos, numeros, guiones y guion bajo."
-			}
+          //Validacion correo
+          if (!valores.correo) {
+            errores.correo = "Ingrese un correo electrónico.";
+          } else if (
+            !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(
+              valores.correo
+            )
+          ) {
+            errores.correo =
+              "El correo solo puede tener letras, puntos, numeros, guiones y guion bajo.";
+          }
 
-			//Validacion de texto
-			if(!valores.texto){
-				errores.texto = "Ingrese su mensaje."
-			}else if(!/^[a-zA-Z ]*$/.test(valores.texto)){
-				errores.texto= "El texto solo puede tener letras A...Z a...z"
-			}
-
-
-			return errores;
-		  }}
-	  	onSubmit={(valores, {resetForm}) => {
-			  resetForm();
-			// console.log("Formulario enviado");
-		  }}
-	  >
-        {({values, errors, touched , handleSubmit, handleChange, handleBlur}) => (
-          <form className="formulario" onSubmit={handleSubmit}>
-			  {/* {console.log(errors)} */}
-			  {/* {console.log(touched)} */}
+          return errores;
+        }}
+        onSubmit={(valores, { resetForm }) => {
+          resetForm();
+          // console.log("Formulario enviado");
+          cambiarFormularioEnv(true);
+          setTimeout(() => cambiarFormularioEnv(false), 3500);
+        }}
+      >
+        {({ errors }) => (
+          <Form className="formulario container">
+            <div>
+              <h2>Contactenos</h2>
+            </div>
+            {/* {console.log(errors)} */}
+            {/* {console.log(touched)} */}
             <div>
               <label htmlFor="nombre">Nombre</label>
-              <input
+              <Field
                 type="text"
                 id="nombre"
                 name="nombre"
                 placeholder="Juan Pablo"
-				value={values.nombre}
-				onChange={handleChange}
-				onBlur={handleBlur}
               />
-			  {touched.nombre && errors.nombre && <div className="error">{errors.nombre}</div>}
+              <ErrorMessage
+                name="nombre"
+                component={() => <div className="error">{errors.nombre}</div>}
+              />
             </div>
             <div>
               <label htmlFor="correo">Correo</label>
-              <input
+              <Field
                 type="email"
                 id="correo"
                 name="correo"
                 placeholder="juanpablo@gmail.com"
-				value={values.correo}
-				onChange={handleChange}
-				onBlur={handleBlur}
               />
-			  {touched.correo && errors.correo && <div className="error">{errors.correo}</div>}
-            </div>
-            <div>
-              <label htmlFor="texto">Texto</label>
-              <input
-                type="text"
-                id="texto"
-                name="texto"
-                placeholder="Deje su mensaje aquí"
-				value={values.texto}
-				onChange={handleChange}
-				onBlur={handleBlur}
+              <ErrorMessage
+                name="correo"
+                component={() => <div className="error">{errors.correo}</div>}
               />
-			  {touched.texto && errors.texto && <div className="error">{errors.texto}</div>}
             </div>
+
+			<div>
+				<label>
+					<Field type="radio" name="sexo" value="Hombre"/> Hombre
+				</label>
+				<label>
+					<Field type="radio" name="sexo" value="Mujer"/> Mujer
+				</label>
+				<label>
+					<Field type="radio" name="sexo" value="Prefiero no decirlo" /> Prefiero no decirlo
+				</label>
+			</div>
+
+			<div>
+				<Field  name="mensaje" as="textarea" placeholder="Escriba aqui su mensaje."/>
+			</div>
+
             <button type="submit">Enviar</button>
-			
-          </form>
+            {formularioEnv && (
+              <p className="exito">Su consulta fue enviada correctamente.</p>
+            )}
+          </Form>
         )}
       </Formik>
     </>
