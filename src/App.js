6b87021af9a0eba3dc2936a5a675113d2e9e97a7
Andy <andysatie@gmail.com>
2021-12-12 14:51:22 -0300

    se maquetó correctamente el app.js individual de la página Principal

--- src/App.js
+++ src/App.js
@@ -2,18 +2,11 @@ import './App.css';
 import PaginaPrincipal from './components/pages/PaginaPrincipal';
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navigation from './components/common/Navigation';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Footer from './components/common/Footer';
 
 const App = () => {
   return (
     <div className="App">
-    <Router>
-      <Navigation></Navigation>
       <PaginaPrincipal/>
-      <Footer></Footer>
-      </Router>
     </div>
   );
 };
