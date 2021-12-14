0ffd0a930f698eb67375351093fe13c5af0c4cdd
Guillermo <gbarazzutti2@gmail.com>
2021-12-13 21:05:04 -0300

    se modificaron los breackpoints en pagina erro404

--- src/components/pages/Error404.js
+++ src/components/pages/Error404.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import diarior from "../img/diarior.jpg"
+import React from "react";
+import diarior from "../img/diarior.jpg";
+import pillow from "../img/pillow.jpg";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Error404 = () => {
-    return (
-        <div>
-            <img src={diarior} alt="Error 404" class="w-100 vh-100" />
-        </div>
-    );
+  return (
+    <div>
+      <img
+        src={diarior}
+        alt="Error 404"
+        class="vh-100 w-100 d-sm-none d-md-block d-lg-block"
+      />
+      <img
+        src={pillow}
+        alt="Error 404"
+        class="vh-100 w-100 d-sm-block d-md-none d-lg-none"
+      />
+    </div>
+  );
 };
 
 export default Error404;
