73f7c564054c0b81ce62e1a4e9d400f65db81a56
abelcg <abelcg86@gmail.com>
2021-12-14 05:14:05 -0300

    se instalo react-quill para la edición de noticia con texto enriquecido y  se agrego el editor

--- src/components/noticias/AgregarNoticia.js
+++ src/components/noticias/AgregarNoticia.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+const modules = {
+  toolbar: [
+    [{ font: [] }],
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    ["bold", "italic", "underline", "strike"],
+    [{ color: [] }, { background: [] }],
+    [{ script: "sub" }, { script: "super" }],
+    ["blockquote", "code-block"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ indent: "-1" }, { indent: "+1" }, { align: [] }],
+    ["link", "image", "video"],
+    ["clean"],
+  ],
+};
 
 const AgregarNoticia = () => {
+  const [value, setValue] = useState("");
+  console.log(typeof value);
+  
+  const createMarkup = ()=> {
+      return {__html: value};
+  }
+
   return (
     <>
       <div class="page-header">
@@ -19,23 +43,38 @@ const AgregarNoticia = () => {
                   type="text"
                   id="postTitle"
                   className="mb-4 form-control"
+                  maxLength="80"
                 ></input>
-                <button type="button" className="mb-4 btn btn-outline-primary">
+                <label className="form-label" for="postImg">
+                  URL de la imagen
+                </label>
+                <input
+                  type="text"
+                  id="postImg"
+                  className="mb-4 form-control"
+                  maxLength="80"
+                ></input>
+                {/*  <button type="button" className="mb-4 btn btn-outline-primary">
                   Agregar Media
-                </button>
+                </button> */}
+                <ReactQuill
+                  modules={modules}
+                  theme="snow"
+                  value={value}
+                  onChange={setValue}
+                  placeholder="El contenido va aqui..."
+                />
               </Card.Body>
             </Card>
           </div>
-            <div className="col-xxl-3 col-lg-4">
-                <Card className="shadow-sm mb-4">
-                <Card.Header>
-                    <h4 className="card-heading">Categorias</h4>
-                </Card.Header>
-                <Card.Body>
-
-                </Card.Body>
-                </Card>
-            </div>
+          <div className="col-xxl-3 col-lg-4">
+            <Card className="shadow-sm mb-4">
+              <Card.Header>
+                <h4 className="card-heading">Categorias</h4>
+              </Card.Header>
+              <Card.Body dangerouslySetInnerHTML = {createMarkup()}></Card.Body>
+            </Card>
+          </div>
         </div>
       </section>
     </>
