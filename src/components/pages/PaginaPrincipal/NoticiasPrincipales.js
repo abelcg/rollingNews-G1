8a0f40460172c5650b8cc4c6a370aaef86ca20c2
abelcg <abelcg86@gmail.com>
2021-12-25 03:40:00 -0300

    se termino de dar estilo a las cards  de pagina principal

--- src/components/pages/PaginaPrincipal/NoticiasPrincipales.js
+++ src/components/pages/PaginaPrincipal/NoticiasPrincipales.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Card, Container, Row, Col } from "react-bootstrap";
 import noticiaPrincipal from "../../../img/noticiaPrincipal.jpg";
 import noticia2 from "../../../img/noticia2.jpg";
-import noticia3 from "../../../img/noticia2.jpg";
+import noticia3 from "../../../img/noticia3.jpg";
 import publicidad1 from "../../../img/publicidad1.jpg";
 import { BsFillRecordCircleFill } from "react-icons/bs";
 
@@ -11,56 +11,35 @@ const NoticiasPrincipales = () => {
     <Container fluid>
       <Row>
         <Col md={9}>
-          {/*  <Card className="my-4">
-            <Row>
-              <Col md={6}>
-                <Card.Body>
-                  <Card.Title className="mb-4">
-                    Pfizer y BioNTech dijeron que su vacuna es "eficaz" contra
-                    Ómicron luego de tres dosis.
-                  </Card.Title>
-                  <hr className='text-danger' />
-                  <Card.Text>
-                    "Probablemente, no queda suficientemente neutralizada
-                    después de dos dosis", comunicó BioNTech sobre la nueva
-                    variante.
-                  </Card.Text>
-                </Card.Body>
-                <Card.Body>
-                  <Card.Link href="#">Ver más...</Card.Link>
-                </Card.Body>
-              </Col>
-              <Col md={6}>
-                <Card.Img
-                  variant="top"
-                  src={noticiaPrincipal}
-                  alt="vacuna pfizer"
-                />
-              </Col>
-            </Row>
-          </Card> */}
           <Card
-           className="card card-overlay-bottom card-grid-lg card-bg-scale h-500 my-4"
-           style={{backgroundImage: `url(${noticiaPrincipal})`,
-           backgroundPosition: "center left",
-           backgroundSize: "cover"}}
+            className="card card-overlay-bottom card-grid-lg card-bg-scale h-500 mt-5 mb-4"
+            style={{
+              backgroundImage: `url(${noticiaPrincipal})`,
+              backgroundPosition: "center left",
+              backgroundSize: "cover",
+            }}
           >
             <div className="card-img-overlay d-flex align-items-center p-3 p-sm-4">
               <div className="w-100 mt-auto">
-                <Card.Link href="#" className="badge bg-danger mb-2 text-decoration-none">
+                <Card.Link
+                  href="#"
+                  className="badge bg-danger mb-2 text-decoration-none"
+                >
                   <BsFillRecordCircleFill className="me-2 small fw-bold"></BsFillRecordCircleFill>
                   Actualidad
                 </Card.Link>
                 <Card.Title className="text-white fs-2">
-                  <Card.Link href="#" className="btn-link stretched-link text-reset text-decoration-none">
-                  Pfizer y BioNTech dijeron que su vacuna es "eficaz" contra
+                  <Card.Link
+                    href="#"
+                    className="btn-link stretched-link text-reset text-decoration-none"
+                  >
+                    Pfizer y BioNTech dijeron que su vacuna es "eficaz" contra
                     Ómicron luego de tres dosis.
                   </Card.Link>
                 </Card.Title>
                 <Card.Text className="text-white">
-                "Probablemente, no queda suficientemente neutralizada
-                    después de dos dosis", comunicó BioNTech sobre la nueva
-                    variante.
+                  "Probablemente, no queda suficientemente neutralizada después
+                  de dos dosis", comunicó BioNTech sobre la nueva variante.
                 </Card.Text>
                 <ul className="nav nav-divider text-white-force align-items-center d-none d-sm-inline-block">
                   <li className="nav-item">
@@ -93,54 +72,138 @@ const NoticiasPrincipales = () => {
         </Col>
         <Col md={3}>
           <img
-            className="w-100 h-auto border border-2 border-danger mt-4"
+            className="w-100 h-auto border border-2 border-danger mt-5"
             src={publicidad1}
             alt="publicidad Mr.Clean"
           />
         </Col>
       </Row>
-      <Row>
+      <Row className="g-4">
         <Col md={6}>
-          <Card className="my-4">
-            <Card.Img variant="bottom" src={noticia2} alt="hombre estresado" />
-            <Card.Body>
-              <Card.Title>
-                ¿Cómo optimizar el tiempo para mejorar la productividad?.
+          <Card className="card bg-transparent h-100 my-4">
+            <div className="position-relative h-100">
+              <img
+                className="card-img w-100 h-100"
+                src={noticia2}
+                alt="hombre estresado"
+              />
+              <div className="card-img-overlay d-flex align-items-start flex-column p-3">
+                <div className="w-100 mt-auto">
+                  <Card.Link
+                    href="#"
+                    className="badge bg-warning mb-2 text-decoration-none"
+                  >
+                    <BsFillRecordCircleFill className="me-2 small fw-bold"></BsFillRecordCircleFill>
+                    Tecnología
+                  </Card.Link>
+                </div>
+              </div>
+            </div>
+            <Card.Body className="px-1 pt-3">
+              <Card.Title className="card-title">
+                <Card.Link
+                  href="#"
+                  className="btn-link text-reset fw-bold text-decoration-none"
+                >
+                  ¿Cómo optimizar el tiempo para mejorar la productividad?
+                </Card.Link>
               </Card.Title>
-              <hr className="text-danger" />
               <Card.Text>
-                Muchas veces sentimos que el tiempo pasa volando...
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+                assumenda eum voluptatem quibusdam, minus veritatis sequi dolor
+                voluptate beatae nostrum accusantium expedita asperiores hic
+                velit commodi? Ipsa quae tempora ducimus!
               </Card.Text>
-            </Card.Body>
-            <Card.Body>
-              <Card.Link href="#">Ver más...</Card.Link>
+              <ul className="nav nav-divider align-items-center d-none d-sm-inline-block">
+                <li className="nav-item">
+                  <div className="nav-link">
+                    <div className="d-flex align-items-center position-relative">
+                      <div className="avatar avatar-xs">
+                        <img
+                          className="avatar-img rounded-circle"
+                          src={publicidad1}
+                          alt="avatar"
+                        />
+                      </div>
+                      <span className="ms-3">
+                        Por{" "}
+                        <a
+                          href="#"
+                          className="stretched-link text-reset btn-link text-decoration-none"
+                        >
+                          Juan Alaniz
+                        </a>
+                      </span>
+                    </div>
+                  </div>
+                </li>
+                <li className="nav-item">| Enero 22, 2021</li>
+              </ul>
             </Card.Body>
           </Card>
         </Col>
         <Col md={6}>
-          <Card className="my-4">
-            <Row>
-              <Card.Img variant="top" src={noticia3} alt="empleado IT" />
-              <Card.Body>
-                <Card.Title>
+          <Card className="card bg-transparent h-100 my-4">
+            <div className="position-relative h-100">
+              <img
+                className="card-img w-100 h-100"
+                src={noticia3}
+                alt="empleado IT"
+              />
+              <div className="card-img-overlay d-flex align-items-start flex-column p-3">
+                <div className="w-100 mt-auto">
+                  <Card.Link
+                    href="#"
+                    className="badge bg-warning mb-2 text-decoration-none"
+                  >
+                    <BsFillRecordCircleFill className="me-2 small fw-bold"></BsFillRecordCircleFill>
+                    Tecnología
+                  </Card.Link>
+                </div>
+              </div>
+            </div>
+            <Card.Body className="px-1 pt-3">
+              <Card.Title className="card-title">
+                <Card.Link
+                  href="#"
+                  className="btn-link text-reset fw-bold text-decoration-none"
+                >
                   Convoca a 900 empleados a la misma reunión por Zoom y los
-                  despide a todos: en apenas un minuto, el CEO de esta empresa
-                  echó al 15% de su plantilla.
-                </Card.Title>
-                <hr className="text-danger" />
-                <Card.Text>
-                  El CEO de la fintech estadounidense Better.com, Vishal Garg,
-                  organizó el pasado miércoles una reunión por Zoom a la que
-                  estaban invitados 900 empleados. En esa misma comunicación a
-                  distancia el dirigente de la compañía les informó de que todos
-                  los que habían sido invitados a la vidoconferencia estaban
-                  despedidos, según la BBC.
-                </Card.Text>
-              </Card.Body>
-              <Card.Body>
-                <Card.Link href="#">Ver más...</Card.Link>
-              </Card.Body>
-            </Row>
+                  despide a todos
+                </Card.Link>
+              </Card.Title>
+              <Card.Text>
+                En apenas un minuto, el CEO de esta empresa echó al 15% de su
+                plantilla.La fintech estadounidense Better.com, Vishal Garg,
+                organizó el pasado miércoles una reunión por Zoom a la que
+                estaban invitados 900 empleados. 
+              </Card.Text>
+              <ul className="nav nav-divider align-items-center d-none d-sm-inline-block">
+                <li className="nav-item">
+                  <div className="nav-link">
+                    <div className="d-flex align-items-center position-relative">
+                      <div className="avatar avatar-xs">
+                        <img
+                          className="avatar-img rounded-circle"
+                          src={publicidad1}
+                          alt="avatar"
+                        />
+                      </div>
+                      <span className="ms-3">
+                        Por{" "}
+                        <a
+                          href="#"
+                          className="stretched-link text-reset btn-link text-decoration-none"
+                        >
+                          Andy Garcia
+                        </a>
+                      </span>
+                    </div>
+                  </div>
+                </li>
+                <li className="nav-item">| Agosto 15, 2021</li>
+              </ul>
+            </Card.Body>
           </Card>
         </Col>
       </Row>
--- src/components/pages/PaginaPrincipal/NoticiasXcategoria.js
+++ src/components/pages/PaginaPrincipal/NoticiasXcategoria.js
@@ -1,113 +1,298 @@
 import React from "react";
 import { Container, Card, Row, Col } from "react-bootstrap";
-import publicidad3 from '../../../img/publicidad3.jpg';
-import publicidad4 from '../../../img/publicidad4.jpg';
-import publicidad2 from '../../../img/publicidad2.jpg';
+import publicidad3 from "../../../img/publicidad3.jpg";
+import publicidad4 from "../../../img/publicidad4.jpg";
+import publicidad2 from "../../../img/publicidad2.jpg";
+import { BsFillRecordCircleFill } from "react-icons/bs";
 
 const NoticiasXcategoria = () => {
   return (
     <Container>
       <hr className="text-danger" />
-      <Row className="my-4">
+      <Row className="my-4 g-2">
         <Col md={12}>
           <div className="text-center my-3">
-        <img className='w-50 h-auto my-2 border' src={publicidad2} alt="publicidad" />
+            <img
+              className="w-50 h-auto my-2 border"
+              src={publicidad2}
+              alt="publicidad"
+            />
           </div>
         </Col>
-        <hr />
+        <hr className="text-danger" />
         <Col md={4}>
-          <h3>Actualidad</h3>
-          <Card>
-            <Card.Body>
+          <h3 className="text-center">Actualidad</h3>
+          <Card className="card mb-4">
+            <Card.Body className="p-4 border rounded-3">
+              <Card.Link href="#" className="badge bg-danger mb-2 text-decoration-none">
+              <BsFillRecordCircleFill className="me-2 small fw-bold"></BsFillRecordCircleFill>
+                Actualidad
+              </Card.Link>
               <Card.Title>
+                <Card.Link href="#" className="btn-link text-reset fw-bold text-decoration-none">
                 Nominan al canciller mexicano Marcelo Ebrard como "persona del
                 año" por su demanda contra fabricantes de armas en EE.UU.
+                </Card.Link>
               </Card.Title>
-              <Card.Text>
+              <Card.Text className="m-0 text-decoration-none">
                 El Gobierno mexicano señala a 11 empresas estadounidenses de
                 promover prácticas comerciales que facilitan el tráfico ilegal
                 de armamento y alimentan la violencia en el país
                 latinoamericano.
               </Card.Text>
-              <Card.Link href="#">Ver más...</Card.Link>
+              <ul className="nav nav-divider align-items-center d-none d-sm-inline-block">
+                <li className="nav-item">
+                  <div className="nav-link">
+                    <div className="d-flex align-items-center position-relative">
+                      <div className="avatar avatar-xs">
+                        <img
+                          className="avatar-img rounded-circle"
+                          src={publicidad2}
+                          alt="avatar"
+                        />
+                      </div>
+                      <span className="ms-3">
+                        Por{" "}
+                        <a href="#" className="stretched-link text-reset btn-link">
+                          Esteban Marquez
+                        </a>
+                      </span>
+                    </div>
+                  </div>
+                </li>
+                <li className="nav-item">Octubre 22, 2021</li>
+              </ul>
             </Card.Body>
           </Card>
-
-          <Card>
-            <Card.Body>
+           <Card className="card mb-4">
+            <Card.Body className="p-4 border rounded-3">
+              <Card.Link href="#" className="badge bg-danger mb-2 text-decoration-none">
+              <BsFillRecordCircleFill className="me-2 small fw-bold"></BsFillRecordCircleFill>
+                Actualidad
+              </Card.Link>
               <Card.Title>
+                <Card.Link href="#" className="btn-link text-reset fw-bold text-decoration-none">
                 El príncipe Enrique anima a la gente a dejar sus trabajos si no
                 les reportan felicidad y la Red no le perdona.
+                </Card.Link>
               </Card.Title>
-              <Card.Text>
-                El duque de Sussex cree que la gente tiene que rechazar un
+              <Card.Text className="m-0 text-decoration-none">
+              El duque de Sussex cree que la gente tiene que rechazar un
                 trabajo que daña a su salud mental.
               </Card.Text>
-              <Card.Link href="#">Ver más...</Card.Link>
+              <ul className="nav nav-divider align-items-center d-none d-sm-inline-block">
+                <li className="nav-item">
+                  <div className="nav-link">
+                    <div className="d-flex align-items-center position-relative">
+                      <div className="avatar avatar-xs">
+                        <img
+                          className="avatar-img rounded-circle"
+                          src={publicidad2}
+                          alt="avatar"
+                        />
+                      </div>
+                      <span className="ms-3">
+                        Por{" "}
+                        <a href="#" className="stretched-link text-reset btn-link">
+                          Esteban Marquez
+                        </a>
+                      </span>
+                    </div>
+                  </div>
+                </li>
+                <li className="nav-item">Octubre 22, 2021</li>
+              </ul>
             </Card.Body>
           </Card>
         </Col>
 
         <Col md={4}>
-          <h3>Espectáculos</h3>
-          <Card>
-            <Card.Body>
+          <h3 className="text-center">Espectáculos</h3>
+           <Card className="card mb-4">
+            <Card.Body className="p-4 border rounded-3">
+              <Card.Link href="#" className="badge bg-danger mb-2 text-decoration-none">
+              <BsFillRecordCircleFill className="me-2 small fw-bold"></BsFillRecordCircleFill>
+              Espectáculos
+              </Card.Link>
               <Card.Title>
+                <Card.Link href="#" className="btn-link text-reset fw-bold text-decoration-none">
                 BTS, Adele y todos los artistas que ganaron en los People's
                 Choice Awards 2021.
+                </Card.Link>
               </Card.Title>
-              <Card.Text>
-                BTS y Adele fueron algunos de los artistas galardonados que más
+              <Card.Text className="m-0 text-decoration-none">
+              BTS y Adele fueron algunos de los artistas galardonados que más
                 destacaron en los People's Choice Awards 2021.
               </Card.Text>
-              <Card.Link href="#">Ver más...</Card.Link>
+              <ul className="nav nav-divider align-items-center d-none d-sm-inline-block">
+                <li className="nav-item">
+                  <div className="nav-link">
+                    <div className="d-flex align-items-center position-relative">
+                      <div className="avatar avatar-xs">
+                        <img
+                          className="avatar-img rounded-circle"
+                          src={publicidad2}
+                          alt="avatar"
+                        />
+                      </div>
+                      <span className="ms-3">
+                        Por{" "}
+                        <a href="#" className="stretched-link text-reset btn-link">
+                          Esteban Marquez
+                        </a>
+                      </span>
+                    </div>
+                  </div>
+                </li>
+                <li className="nav-item">Octubre 22, 2021</li>
+              </ul>
             </Card.Body>
           </Card>
-          <Card>
-            <Card.Body>
+          <Card className="card mb-4">
+            <Card.Body className="p-4 border rounded-3">
+              <Card.Link href="#" className="badge bg-danger mb-2 text-decoration-none">
+              <BsFillRecordCircleFill className="me-2 small fw-bold"></BsFillRecordCircleFill>
+              Espectáculos
+              </Card.Link>
               <Card.Title>
+                <Card.Link href="#" className="btn-link text-reset fw-bold text-decoration-none">
                 The Walking Dead revela el secreto del origen de los zombies en
-                la serie.
+                la serie
+                </Card.Link>
               </Card.Title>
-              <Card.Text>
-                La serie spinoff <b>"The Walking Dead: World Beyond"</b>{" "}
+              <Card.Text className="m-0 text-decoration-none">
+              La serie spinoff <b>"The Walking Dead: World Beyond"</b>{" "}
                 finalmente reveló cómo surgió el <b>virus zombie</b> que casi
                 extermina a la humanidad. Descubrilo aquí.
               </Card.Text>
-              <Card.Link href="#">Ver más...</Card.Link>
+              <ul className="nav nav-divider align-items-center d-none d-sm-inline-block">
+                <li className="nav-item">
+                  <div className="nav-link">
+                    <div className="d-flex align-items-center position-relative">
+                      <div className="avatar avatar-xs">
+                        <img
+                          className="avatar-img rounded-circle"
+                          src={publicidad2}
+                          alt="avatar"
+                        />
+                      </div>
+                      <span className="ms-3">
+                        Por{" "}
+                        <a href="#" className="stretched-link text-reset btn-link">
+                          Esteban Marquez
+                        </a>
+                      </span>
+                    </div>
+                  </div>
+                </li>
+                <li className="nav-item">Octubre 22, 2021</li>
+              </ul>
             </Card.Body>
           </Card>
         </Col>
 
         <Col md={4}>
-          <h3>Tecnología</h3>
-          <Card>
-            <Card.Body>
-              <Card.Title>Estas gafas ayudaron a un hombre ciego a volver a ver tras 40 años.</Card.Title>
-              <Card.Text>
-                Un tratamiento sin precedentes de GenSight Biologics le devolvió parcialmente la vista a un paciente <b>ciego</b>, al convinar la terapia génica con unas gafas especializadas.
+          <h3 className="text-center">Tecnología</h3>
+           <Card className="card mb-4">
+            <Card.Body className="p-4 border rounded-3">
+              <Card.Link href="#" className="badge bg-danger mb-2 text-decoration-none">
+              <BsFillRecordCircleFill className="me-2 small fw-bold"></BsFillRecordCircleFill>
+              Tecnología
+              </Card.Link>
+              <Card.Title>
+                <Card.Link href="#" className="btn-link text-reset fw-bold text-decoration-none">
+                Estas gafas ayudaron a un hombre ciego a volver a ver tras 40
+                años
+                </Card.Link>
+              </Card.Title>
+              <Card.Text className="m-0 text-decoration-none">
+              Un tratamiento sin precedentes de GenSight Biologics le devolvió
+                parcialmente la vista a un paciente <b>ciego</b>, al convinar la
+                terapia génica con unas gafas especializadas.
               </Card.Text>
-              <Card.Link href="#">Ver más...</Card.Link>
+              <ul className="nav nav-divider align-items-center d-none d-sm-inline-block">
+                <li className="nav-item">
+                  <div className="nav-link">
+                    <div className="d-flex align-items-center position-relative">
+                      <div className="avatar avatar-xs">
+                        <img
+                          className="avatar-img rounded-circle"
+                          src={publicidad2}
+                          alt="avatar"
+                        />
+                      </div>
+                      <span className="ms-3">
+                        Por{" "}
+                        <a href="#" className="stretched-link text-reset btn-link">
+                          Esteban Marquez
+                        </a>
+                      </span>
+                    </div>
+                  </div>
+                </li>
+                <li className="nav-item">Octubre 22, 2021</li>
+              </ul>
             </Card.Body>
           </Card>
-          <Card>
-            <Card.Body>
-              <Card.Title>La NASA lanza una demostración de un láser que podría revolucionar la comunicación espacial.</Card.Title>
-              <Card.Text>
-                La demostración del retransmisor de comunicaciones láser (LCRD, por sus siglas en inglés) de la NASA podría revolucionar la forma en que la agencia se comunica con futuras misiones en todo el sistema solar.
+          <Card className="card mb-4">
+            <Card.Body className="p-4 border rounded-3">
+              <Card.Link href="#" className="badge bg-danger mb-2 text-decoration-none">
+              <BsFillRecordCircleFill className="me-2 small fw-bold"></BsFillRecordCircleFill>
+              Tecnología
+              </Card.Link>
+              <Card.Title>
+                <Card.Link href="#" className="btn-link text-reset fw-bold text-decoration-none">
+                La NASA lanza una demostración de un láser que podría
+                revolucionar la comunicación espacial
+                </Card.Link>
+              </Card.Title>
+              <Card.Text className="m-0 text-decoration-none">
+              La demostración del retransmisor de comunicaciones láser (LCRD,
+                por sus siglas en inglés) de la NASA podría revolucionar la
+                forma en que la agencia se comunica con futuras misiones en todo
+                el sistema solar
               </Card.Text>
-              <Card.Link href="#">Ver más... </Card.Link>
+              <ul className="nav nav-divider align-items-center d-none d-sm-inline-block">
+                <li className="nav-item">
+                  <div className="nav-link">
+                    <div className="d-flex align-items-center position-relative">
+                      <div className="avatar avatar-xs">
+                        <img
+                          className="avatar-img rounded-circle"
+                          src={publicidad2}
+                          alt="avatar"
+                        />
+                      </div>
+                      <span className="ms-3">
+                        Por{" "}
+                        <a href="#" className="stretched-link text-reset btn-link">
+                          Esteban Marquez
+                        </a>
+                      </span>
+                    </div>
+                  </div>
+                </li>
+                <li className="nav-item">Octubre 22, 2021</li>
+              </ul>
             </Card.Body>
           </Card>
         </Col>
       </Row>
       <Row>
-        <hr />
+        <hr className="text-danger"/>
         <Col md={6}>
-          <img className='w-100 h-auto my-2' src={publicidad3} alt="publicidad" />
+          <img
+            className="w-100 h-auto my-2"
+            src={publicidad3}
+            alt="publicidad"
+          />
         </Col>
         <Col md={6}>
-          <img className='w-100 h-auto my-2' src={publicidad4} alt="publicidad" />
+          <img
+            className="w-100 h-auto my-2"
+            src={publicidad4}
+            alt="publicidad"
+          />
         </Col>
       </Row>
     </Container>
